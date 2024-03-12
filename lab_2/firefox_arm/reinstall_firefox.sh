5e29c37d2a978538e612078df1d7606995717f75
Pablo Barbecho <58553873+Pbarbecho@users.noreply.github.com>
2024-03-12 10:43:14 +0100

    Add files via upload

---
+++ lab_2/firefox_arm/reinstall_firefox.sh
@@ -0,0 +1,15 @@
+sudo snap disable firefox
+
+sudo snap remove --purge firefox
+
+cd ~/snap/
+
+sudo rm -r firefox/
+
+sudo apt purge firefox
+
+sudo add-apt-repository ppa:mozillateam/ppa
+
+sudo apt-get update
+
+sudo apt install -y firefox
