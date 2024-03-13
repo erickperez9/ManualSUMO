c4eac58e88d4cd7411567f18bba9c77041ecf50f
Pablo Barbecho <58553873+Pbarbecho@users.noreply.github.com>
2024-03-13 13:28:11 +0100

    Add files via upload

---
+++ lab_5/rf_lr.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+import seaborn as sns
+import numpy as np
+# Cargar el archivo CSV
+df = pd.read_csv('/Users/Pablo_1/Dropbox/Mac/Documents/UCUENCA/SEMESTRE_II/POSGRADO/Redes Industriales/LABs/pythonProject/tripinfo.csv')
+
+# Eliminar filas con valores faltantes en las columnas relevantes
+#df.dropna(subset=['emissions_CO2_abs', 'tripinfo_vType','tripinfo_arrivalSpeed','tripinfo_routeLength'], inplace=True)
+df.dropna(subset=['emissions_CO2_abs', 'tripinfo_vType','tripinfo_arrivalSpeed','tripinfo_routeLength','emissions_fuel_abs','tripinfo_timeLoss'], inplace=True)
+
+# Seleccionar características (emisiones CO2 absolutas) y etiquetas (tipo de vehículo)
+#X = df[['emissions_CO2_abs','tripinfo_arrivalSpeed','tripinfo_routeLength']]
+X = df[['tripinfo_arrivalSpeed','emissions_fuel_abs','tripinfo_routeLength','tripinfo_timeLoss']]
+y = df['tripinfo_vType']
+
+# Codificar las etiquetas de texto a números
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0, stratify=y)
+
+# Definir diccionario para mapear los números de clase a tipos de vehículos
+clase_vehiculo = {0: 'bus', 1: 'taxi', 2: 'motorcycle'}
+
+# Inicializar y entrenar el modelo RandomForestClassifier
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model.fit(X_train, y_train)
+
+# Inicializar y entrenar el modelo LogisticRegression
+lr_model = LogisticRegression(max_iter=1000, random_state=42)
+lr_model.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred_rf = rf_model.predict(X_test)
+y_pred_lr = lr_model.predict(X_test)
+
+# Evaluar el rendimiento de Random Forest
+print("Random Forest Classifier:")
+print("Accuracy Score:", accuracy_score(y_test, y_pred_rf))
+print("\nClassification Report:\n", classification_report(y_test, y_pred_rf))
+
+# Evaluar el rendimiento de Logistic Regression
+print("\nLogistic Regression:")
+print("Accuracy Score:", accuracy_score(y_test, y_pred_lr))
+print("\nClassification Report:\n", classification_report(y_test, y_pred_lr))
